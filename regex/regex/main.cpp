2509975154fccefc87bf465c07e728565ac415d3
huygens-cheng <1550191474@qq.com>
2022-11-24 22:27:25 +0800

    测试regex_match

--- regex/regex/main.cpp
+++ regex/regex/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include <regex>
 using namespace std;
 
 int main()
 {
+    regex reg("<.*>.*</.*>");
+    bool bMatch=regex_match("<tag>value</tag>",reg);
+
+    cout<<"match result: "<<boolalpha<<bMatch<<endl;
     cout << "Hello World!" << endl;
     return 0;
 }
